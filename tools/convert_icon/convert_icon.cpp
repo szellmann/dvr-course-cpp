df7c222dbfd83f9ea3945757379be6c34977e20a
Stefan Zellmann <info@szellmann.de>
2026-01-08 19:34:05 +0100

    Rewrite ICON tool to compute z from HSURF and HHL

--- tools/convert_icon/convert_icon.cpp
+++ tools/convert_icon/convert_icon.cpp
@@ -14,6 +14,10 @@
 #endif
 
 struct {
+  std::string horizontalGridFile; // -hgrid
+  std::string hsurfFile; // -hsurf
+  std::vector<std::string> hhlFiles; // -hhl
+  std::vector<std::string> dataFiles; // -data
   bool convertToIC{true};
   bool convertToUMesh{false};
 } g_appState;
@@ -50,7 +54,7 @@ inline umesh::vec3f toCartesian(const umesh::vec3f spherical)
 static size_t readDimLength(int ncid, std::string name) {
   int retval, dimid;
   if ((retval != nc_inq_dimid(ncid, name.c_str(), &dimid)) != NC_NOERR) {
-    fprintf(stderr, "dim %s not found\n", name.c_str());
+    fprintf(stderr, "dim %s not found: %s\n", name.c_str(), nc_strerror(retval));
     return ~0ull;
   }
 
@@ -108,6 +112,49 @@ static std::vector<double> readDoubleVar(int ncid, std::string name, size_t len)
 }
 
 
+static void parseCommandLine(int argc, char *argv[]) {
+  enum Mode { Hgrid, Hsurf, Hhl, Data, None, };
+  Mode mode{None};
+
+  for (int i=1; i<argc; ++i) {
+    std::string arg = argv[i];
+    if (arg[0] != '-') {
+      if (mode == Hgrid) {
+        g_appState.horizontalGridFile = arg;
+      }
+      else if (mode == Hsurf) {
+        g_appState.hsurfFile = arg;
+      }
+      else if (mode == Hhl) {
+        g_appState.hhlFiles.push_back(arg);
+      }
+      else if (mode == Data) {
+        g_appState.dataFiles.push_back(arg);
+      }
+      else {
+        fprintf(stderr, "Unknown parm: %s\n", argv[i]);
+        break;
+      }
+    }
+    else if (arg == "-hgrid") {
+      mode = Hgrid;
+    }
+    else if (arg == "-hsurf") {
+      mode = Hsurf;
+    }
+    else if (arg == "-hhl") {
+      mode = Hhl;
+    }
+    else if (arg == "-data") {
+      mode = Data;
+    }
+  }
+}
+
+static void printUsage() {
+  fprintf(stderr, "%s\n",
+    "Usage: ./convert_icon -hgrid <hg.nc> -hsurf <hs.nc> -hhl [hh.nc*] -data [df.nc*]");
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 3 || std::string(argv[1]) == "help" ) {
@@ -115,8 +162,20 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  parseCommandLine(argc, argv);
+
+  if (g_appState.horizontalGridFile.empty() ||
+      g_appState.hsurfFile.empty() ||
+      g_appState.hhlFiles.empty() || g_appState.hsurfFile.empty()) {
+    printUsage();
+    return 1;
+  }
+
   int ncid, retval;
-  if ((retval = nc_open(argv[1], NC_NOWRITE, &ncid)) != NC_NOERR) {
+
+  // Horizontal grid file:
+
+  if ((retval = nc_open(g_appState.horizontalGridFile.c_str(), NC_NOWRITE, &ncid)) != NC_NOERR) {
     printf("Error opening file: %s\n", nc_strerror(retval));
     return 1;
   }
@@ -142,15 +201,83 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // Data files:
+  struct DataField {
+    int height{0};
+    std::vector<float> value;
+  };
 
-  int numLayers = 0;
-  std::vector<float> heights, values;
-  for (int i=2; i<argc; ++i) {
-    if ((retval = nc_open(argv[i], NC_NOWRITE, &ncid)) != NC_NOERR) {
+  // HSURF file:
+
+  if ((retval = nc_open(g_appState.hsurfFile.c_str(), NC_NOWRITE, &ncid)) != NC_NOERR) {
+    printf("Error opening file: %s\n", nc_strerror(retval));
+    return 1;
+  }
+
+  auto hsurf = readDoubleVar(ncid, "HSURF", cell);
+
+  if (clon_vertices.empty() || clat_vertices.empty()) {
+    fprintf(stderr, "%s\n", "Cannot proceed as HSURF is ill-formed");
+    nc_close(ncid);
+    return 1;
+  }
+
+
+  // HHL files:
+
+  std::vector<DataField> hhl(g_appState.hhlFiles.size());
+  int hhlHeightBounds[2] = {INT_MAX, INT_MIN};
+
+  for (int i=0; i<g_appState.hhlFiles.size(); ++i) {
+    std::string hhlFile = g_appState.hhlFiles[i];
+    if ((retval = nc_open(hhlFile.c_str(), NC_NOWRITE, &ncid)) != NC_NOERR) {
       printf("Error opening file: %s\n", nc_strerror(retval));
       return 1;
     }
+
+    DataField &field = hhl[i];
+
+    auto height = readDoubleVar(ncid, "height", 1);
+    if (height.empty()) {
+      fprintf(stderr, "No height found in %s, aborting...\n", hhlFile.c_str());
+      nc_close(ncid);
+      return 1;
+    }
+
+    field.height = (int)height[0];
+
+    hhlHeightBounds[0] = std::min(hhlHeightBounds[0],field.height);
+    hhlHeightBounds[1] = std::max(hhlHeightBounds[1],field.height);
+
+    auto hhlI = readDoubleVar(ncid, "HHL", cell);
+    if (hhlI.empty()) {
+      fprintf(stderr, "HHL not propery read from %s, aborting...\n", hhlFile.c_str());
+      nc_close(ncid);
+      return 1;
+    }
+
+    for (int j=0; j<cell; ++j) {
+      field.value.push_back((float)hhlI[j]);
+    }
+
+    nc_close(ncid);
+  }
+
+  std::sort(hhl.begin(), hhl.end(), [](auto &a, auto &b) { return a.height > b.height; });
+
+
+  // Data files:
+
+  std::vector<DataField> values(g_appState.dataFiles.size());
+  int valuesHeightBounds[2] = {INT_MAX, INT_MIN};
+
+  for (int i=0; i<g_appState.dataFiles.size(); ++i) {
+    std::string dataFile = g_appState.dataFiles[i];
+    if ((retval = nc_open(dataFile.c_str(), NC_NOWRITE, &ncid)) != NC_NOERR) {
+      printf("Error opening file: %s\n", nc_strerror(retval));
+      return 1;
+    }
+
+    DataField &field = values[i];
 
     // read number of cells:
     size_t ncells = readDimLength(ncid, "ncells");
@@ -158,11 +285,15 @@ int main(int argc, char *argv[]) {
 
     auto height = readDoubleVar(ncid, "height", 1);
     if (height.empty()) {
-      fprintf(stderr, "No height found in %s, aborting...\n", argv[i]);
+      fprintf(stderr, "No height found in %s, aborting...\n", dataFile.c_str());
       nc_close(ncid);
       return 1;
     }
-    heights.push_back(height[0]);
+
+    field.height = (int)height[0];
+
+    valuesHeightBounds[0] = std::min(valuesHeightBounds[0],field.height);
+    valuesHeightBounds[1] = std::max(valuesHeightBounds[1],field.height);
 
     // read VARIABLE
     const char *varname = "pres";
@@ -186,47 +317,45 @@ int main(int argc, char *argv[]) {
       var[j] /= maxValue-minValue;
     }
 #endif
-    for (int j=0; j<ncells; ++j) {
-      values.push_back((float)var[j]);
+
+    for (int j=0; j<cell; ++j) {
+      field.value.push_back((float)var[j]);
     }
 
-    numLayers++;
+    nc_close(ncid);
   }
 
-  printf("num layers: %i\n",numLayers);
+  std::sort(values.begin(), values.end(), [](auto &a, auto &b) { return a.height > b.height; });
 
-  std::vector<std::pair<float,int>> height_to_index;
-  for (int i=0; i<numLayers; ++i) {
-    height_to_index.push_back({heights[i],i});
-  }
-  std::sort(height_to_index.begin(),height_to_index.end(),
-    [](auto p1, auto p2) { return p1.first < p2.first; });
-  for (int i=0; i<numLayers; ++i) {
-    std::cout << height_to_index[i].second << '\n';
+  if (!(hhlHeightBounds[0] == valuesHeightBounds[0] && hhlHeightBounds[1] == valuesHeightBounds[1])) {
+    fprintf(stderr, "%s\n", "Heights of HHL and data field don't match, aborting...");
   }
 
+  int heightBounds[2] = { hhlHeightBounds[0], hhlHeightBounds[1] };
 
-  if (numLayers > 30) {
-    std::cerr << "Only loading the first 30 layers..\n";
-    numLayers = 30;
+  int numLayers(g_appState.dataFiles.size());
+
+  if (numLayers > 31) {
+    numLayers = 31;
   }
 
   if (g_appState.convertToIC) {
     std::ofstream out("out.ic",std::ios::binary);
-    for (int i=0; i<cell; ++i) {
-      float lat[3]{(float)clat_vertices[i*3],(float)clat_vertices[i*3+1],(float)clat_vertices[i*3+2]};
-      float lon[3]{(float)clon_vertices[i*3],(float)clon_vertices[i*3+1],(float)clon_vertices[i*3+2]};;
+    for (int cellID=0; cellID<cell; ++cellID) {
+      float lat[3]{(float)clat_vertices[cellID*3],(float)clat_vertices[cellID*3+1],(float)clat_vertices[cellID*3+2]};
+      float lon[3]{(float)clon_vertices[cellID*3],(float)clon_vertices[cellID*3+1],(float)clon_vertices[cellID*3+2]};
+      constexpr float R = 6.371229E6f;
       float H[32];
-      H[0] = 6.371229f;
-      for (int j=0; j<numLayers; ++j) {
-        //H[j+1] = H[j]+height_to_index[j].first/100000.f;
-        H[j+1] = H[j]+height_to_index[j].first/1000.f;
+      H[0] = R + hsurf[cellID];
+      for (int j=1; j<=numLayers; ++j) {
+        H[j] = R + hhl[j].value[cellID]-hsurf[cellID];
       }
+      // for (int j=0; j<=numLayers; ++j) {
+      //   H[j] /= 1000.f;
+      // }
       float value[32];
       for (int j=0; j<numLayers; ++j) {
-        int h = height_to_index[j].second;
-        if (i==0) std::cout << "layer " << j << " is at index " << h << ", height value is " << height_to_index[j].first << '\n';
-        value[j] = values[h*cell+i];
+        value[j] = values[j].value[cellID];
       }
       out.write((const char *)lat,sizeof(lat));
       out.write((const char *)lon,sizeof(lon));
@@ -238,20 +367,19 @@ int main(int argc, char *argv[]) {
   }
 
   if (g_appState.convertToUMesh) {
+#if 0
     using namespace umesh;
     auto output = std::make_shared<UMesh>();
     output->perVertex = std::make_shared<Attribute>();
-    for (int i=0; i<cell; ++i) {
-      float lat[3]{(float)clat_vertices[i*3],(float)clat_vertices[i*3+1],(float)clat_vertices[i*3+2]};
-      float lon[3]{(float)clon_vertices[i*3],(float)clon_vertices[i*3+1],(float)clon_vertices[i*3+2]};;
-      //float H[32];
-      //H[0] = 6.371229f;
-      //for (int j=0; j<numLayers; ++j) {
-      //  H[j+1] = H[j]+height_to_index[j].first/100000.f;
-      //}
+    for (int cellID=0; cellID<cell; ++cellID) {
+      float lat[3]{(float)clat_vertices[cellID*3],(float)clat_vertices[cellID*3+1],(float)clat_vertices[cellID*3+2]};
+      float lon[3]{(float)clon_vertices[cellID*3],(float)clon_vertices[cellID*3+1],(float)clon_vertices[cellID*3+2]};;
       //float value[32];
       float h1 = 6.371229f;
       for (int j=0; j<numLayers; ++j) {
+        int layerHeight = hhl[j].height-1;
+        float h = (j==0) ? h1+hsurf[cellID] : hhl[hhlHeightToIndex[layerHeight]].value[cellID]-hsurf[cellID];
+        if (j == 0)
         float h2 = h1 + height_to_index[j].first/1000.f;
 
         int h = height_to_index[j].second;
@@ -294,5 +422,6 @@ int main(int argc, char *argv[]) {
     std::cout << output->vertices.size() << '\n';
     std::cout << output->wedges.size() << '\n';
     output->saveTo("out.umesh");
+#endif
   }
 }
