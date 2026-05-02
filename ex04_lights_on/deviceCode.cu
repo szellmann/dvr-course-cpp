101cf50dcc1759b90f4464339d446d7fa1a1f2c9
Stefan Zellmann <info@szellmann.de>
2026-05-02 13:48:23 +0200

    Initialize ex04_lights_on from Woodcock sample

---
+++ ex04_lights_on/deviceCode.cu
@@ -0,0 +1,145 @@
+// ======================================================================== //
+// Copyright 2025-2025 Stefan Zellmann                                      //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+// ======================================================================== //
+
+// nanovdb
+#include <nanovdb/math/SampleFromVoxels.h>
+// common
+#include <dvr_course-common.cuh>
+// ex04
+#include "Params.h"
+
+using namespace dvr_course;
+
+// ========================================================
+// device code for example 04: lights_on
+// ========================================================
+namespace ex04_lights_on {
+
+extern "C" __constant__ LaunchParams optixLaunchParams;
+
+// ========================================================
+// Helpers
+// ========================================================
+inline  __device__ Ray generateRay(const vec2f screen, Random &rnd)
+{
+  auto &lp = optixLaunchParams;
+  vec3f org = lp.camera.org;
+  vec3f dir
+    = lp.camera.dir_00
+    + (screen.u+rnd()) * lp.camera.dir_du
+    + (screen.v+rnd()) * lp.camera.dir_dv;
+  dir = normalize(dir);
+  if (fabsf(dir.x) < 1e-5f) dir.x = 1e-5f;
+  if (fabsf(dir.y) < 1e-5f) dir.y = 1e-5f;
+  if (fabsf(dir.z) < 1e-5f) dir.z = 1e-5f;
+  return Ray(org,dir,0.f,1e10f);
+}
+
+inline __device__ bool sampleVolume(const Volume &vol, vec3f pos, float &value)
+{
+  // sample nvdb volume:
+  auto acc = vol.handle->getAccessor();
+  nanovdb::math::Vec3<float> nvdbPos(pos.x,pos.y,pos.z);
+  if (!vol.filterLinear) {
+    auto smp = nanovdb::math::createSampler<0>(acc);
+    value = smp(vol.handle->worldToIndexF(nvdbPos));
+  } else {
+    auto smp = nanovdb::math::createSampler<1>(acc);
+    value = smp(vol.handle->worldToIndexF(nvdbPos));
+  }
+  return true;
+}
+
+inline __device__ vec4f postClassify(Transfunc tf, float v)
+{
+  v = (v - tf.valueRange.lower) / (tf.valueRange.upper - tf.valueRange.lower);
+  int idx = v*(tf.size);
+  float frac = (v*tf.size)-idx;
+  vec4f v1 = tf.values[clamp(idx,0,tf.size-1)];
+  vec4f v2 = tf.values[clamp(idx+1,0,tf.size-1)];
+  return v1*frac+v2*(1.f-frac);
+}
+
+// ========================================================
+// Main ray gen prog (woodcock tracking, A+E)
+// ========================================================
+RAYGEN_PROGRAM(woodcockTrackingAE)()
+{
+  auto &lp = optixLaunchParams;
+  const vec2i threadIndex = getLaunchIndex();
+  const vec2i launchDim = getLaunchDims();
+  const int pixelID = threadIndex.x + getLaunchDims().x * threadIndex.y;
+
+  Random rnd(lp.accumID*launchDim.x*launchDim.y+(unsigned)threadIndex.x,
+             (unsigned)threadIndex.y);
+
+  Ray ray = generateRay(vec2f(threadIndex)+vec2f(.5f), rnd);
+
+  float t0, t1;
+  if (!boxTest(ray, lp.volume.bounds, t0, t1))
+    return;
+
+  ray.tmin = t0, ray.tmax = t1;
+
+  vec3f albedo = 0.f;
+  float extinction = 0.f;
+
+  const float majorant = 1.f;
+
+  float t=ray.tmin;
+
+  while (1) {
+    // In later chapters majorants will vary in space:
+    if (majorant <= 0.f)
+      break;
+
+    t -= (logf(1.f - rnd()) / (majorant / lp.unitDistance));
+
+    if (t > ray.tmax)
+      break;
+
+    vec3f P = ray.org+ray.dir*t;
+
+    float value{0.f};
+    if (!sampleVolume(lp.volume, P, value))
+      continue;
+
+    vec4f sample = postClassify(lp.transfunc, value);
+    float u = rnd();
+    if (sample.w >= u * majorant) {
+      albedo = vec3f(sample.x,sample.y,sample.z);
+      extinction = sample.w;
+      break;
+    }
+  }
+
+  vec3f color = albedo * lp.ambientColor * lp.ambientRadiance;
+  float alpha = extinction > 0.f ? 1.f : 0.f;
+
+  float accum = 1.f/(lp.accumID+1);
+  lp.accumBuffer[pixelID] = lerp(vec4f(color,alpha), lp.accumBuffer[pixelID], accum);
+
+  vec4f accumColor = lp.accumBuffer[pixelID];
+  accumColor.r = linear_to_srgb(accumColor.r);
+  accumColor.g = linear_to_srgb(accumColor.g);
+  accumColor.b = linear_to_srgb(accumColor.b);
+  lp.fbPointer[pixelID] = make_rgba(accumColor);
+}
+
+} // namespace ex04_lights_on
+
+
+
---
+++ ex04_lights_on/hostCode.cpp
@@ -0,0 +1,175 @@
+// std
+#include <fstream>
+#include <string>
+
+// nanovdb
+#include <nanovdb/GridHandle.h>
+#include <nanovdb/HostBuffer.h>
+#include <nanovdb/NanoVDB.h>
+#include <nanovdb/io/IO.h>
+#include <nanovdb/tools/GridStats.h>
+
+// Header with common resources; .h: host, .cuh: device
+#include <dvr_course-common.h>
+
+// ex04:
+#include "Params.h"
+#ifdef RTCORE
+#include "Params-owl.h"
+#endif
+
+// common namespace for helper classes:
+// Camera, FB, wrappers for RTX execution model, etc. etc.
+using namespace dvr_course;
+
+DECL_LAUNCH_PARAMS(ex04_lights_on::LaunchParams)
+
+struct {
+  std::string filepath;
+  Transfunc transfunc;
+  float unitDistance;
+} g_appState;
+
+namespace ex04_lights_on {
+#ifdef RTCORE
+extern "C" char ptxCode[];
+#else
+extern void woodcockTrackingAE();
+#endif
+
+void printUsage() {
+  fprintf(stderr, "%s", "Usage: ex04_lights_on file.nvdb\n");
+}
+
+static void parseCommandLine(int argc, char *argv[]) {
+
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+    if (arg[0] != '-' && endsWith(arg,".nvdb"))
+      g_appState.filepath = arg;
+  }
+}
+
+extern "C" int main(int argc, char *argv[]) {
+
+  if (argc < 2) {
+    printUsage();
+    exit(-1);
+  }
+
+  parseCommandLine(argc, argv);
+
+  if (g_appState.filepath.empty()) {
+    printUsage();
+    exit(-1);
+  }
+
+  nanovdb::GridHandle<nanovdb::HostBuffer> gridHandle;
+
+  try {
+    auto grid = nanovdb::io::readGrid(g_appState.filepath);
+    auto hostbuffer = nanovdb::HostBuffer::create(grid.bufferSize());
+    std::memcpy(hostbuffer.data(), grid.data(), grid.bufferSize());
+    gridHandle = std::move(hostbuffer);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+    printUsage();
+    exit(-1);
+  }
+
+  Buffer deviceGrid(gridHandle.bufferSize(), (uint8_t *)gridHandle.data());
+
+  auto boundsMin = gridHandle.gridMetaData()->worldBBox().min();
+  auto boundsMax = gridHandle.gridMetaData()->worldBBox().max()-nanovdb::Vec3d(1.f);
+  box3f volbounds({(float)boundsMin[0], (float)boundsMin[1], (float)boundsMin[2]},
+                  {(float)boundsMax[0], (float)boundsMax[1], (float)boundsMax[2]});
+
+  Pipeline pl(argc, argv, "ex04_lights_on");
+
+  int imgWidth=512, imgHeight=512;
+  Frame fb(imgWidth, imgHeight);
+  pl.setFrame(&fb);
+
+  Camera cam;
+  cam.viewAll(volbounds);
+  pl.setCamera(&cam);
+
+  if (!pl.transfuncValid()) {
+    auto &tf = g_appState.transfunc;
+    tf.valueRange = {gridHandle.grid<float>()->tree().root().minimum(),
+                     gridHandle.grid<float>()->tree().root().maximum()};
+
+    tf.valueRange.lower
+      = fminf(tf.valueRange.lower, gridHandle.grid<float>()->tree().root().background());
+    tf.valueRange.upper
+      = fmaxf(tf.valueRange.upper, gridHandle.grid<float>()->tree().root().background());
+
+    if (tf.valueRange.empty()) tf.valueRange = {0.f,1.f};
+    tf.setLUT(std::vector<vec4f>({
+      {0.f,0.f,1.f,0.1f },
+      {0.f,1.f,0.f,0.1f }
+    }));
+    pl.setTransfunc(&tf);
+  }
+
+  g_appState.unitDistance = 1.0f;
+  pl.uiParam("Unit distance", &g_appState.unitDistance, 0.001f, 5.f);
+
+#ifdef RTCORE
+  pl.setRayGen(ptxCode, "woodcockTrackingAE");
+  pl.setLaunchParamsDecl(launchParams_owl, sizeof(LaunchParams));
+#else
+  pl.setRayGen(woodcockTrackingAE);
+#endif
+
+  LaunchParams parms;
+
+  // volume
+  pl.launchParam("volume.handle", (RawPointer &)parms.volume.handle) = (nanovdb::NanoGrid<float> *)deviceGrid.data();
+  pl.launchParam("volume.filterLinear", parms.volume.filterLinear) = true;
+  pl.launchParam("volume.bounds", parms.volume.bounds) = volbounds;
+  // lighting
+  pl.launchParam("ambientColor", parms.ambientColor) = vec3f(1.f);
+  pl.launchParam("ambientRadiance", parms.ambientRadiance) = 1.f;
+
+  // Render and present...
+  // For default (PNG image) pipeline this
+  // loop returns immediately
+  do {
+    struct {
+      vec3f lower_left, horizontal, vertical;
+    } screen;
+    cam.getScreen(screen.lower_left,screen.horizontal,screen.vertical);
+
+    // update camera:
+    pl.launchParam("camera.org", parms.camera.org) = cam.getPosition();
+    pl.launchParam("camera.dir_00", parms.camera.dir_00) = screen.lower_left;
+    pl.launchParam("camera.dir_du", parms.camera.dir_du) = screen.horizontal / imgWidth;
+    pl.launchParam("camera.dir_dv", parms.camera.dir_dv) = screen.vertical / imgHeight;
+    // update transfunc:
+    pl.launchParam("transfunc.valueRange", parms.transfunc.valueRange) = pl.getTransfunc()->valueRange;
+    pl.launchParam("transfunc.size", parms.transfunc.size) = pl.getTransfunc()->size;
+    pl.launchParam("transfunc.values", (RawPointer &)parms.transfunc.values) = pl.getTransfunc()->rgbaLUT;
+    // update framebuffer:
+    pl.launchParam("fbPointer", (RawPointer &)parms.fbPointer) = fb.fbPointer;
+    pl.launchParam("fbDepth", (RawPointer &)parms.fbDepth) = fb.fbDepth;
+    pl.launchParam("accumBuffer", (RawPointer &)parms.accumBuffer) = fb.accumBuffer;
+    // update DVR params:
+    pl.launchParam("unitDistance", parms.unitDistance) = g_appState.unitDistance;
+    // update accum:
+    pl.launchParam("accumID", parms.accumID) = pl.frameID;
+
+    // set params:
+    SET_LAUNCH_PARAMS(parms);
+
+    pl.launch();
+    pl.present();
+  } while (pl.isRunning());
+
+  return 0;
+}
+
+} // namespace ex04_lights_on
+
+
+
