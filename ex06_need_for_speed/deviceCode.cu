fcebad3be5a80a5bc5c2e67fe080dd1c85455e93
Stefan Zellmann <info@szellmann.de>
2026-06-11 10:15:07 +0100

    Remove unused code

--- ex06_need_for_speed/deviceCode.cu
+++ ex06_need_for_speed/deviceCode.cu
@@ -416,31 +416,6 @@ OPTIX_CLOSEST_HIT_PROGRAM(TetClosestHit)()
 #endif
 
 // ========================================================
-// Sphere intersection, used for the makeshift
-// traversal structure
-// ========================================================
-inline __device__
-bool intersectSphere(const Ray &ray, float radius, float &tnear, float &tfar) {
-  float A = dot(ray.dir,ray.dir);
-  float B = dot(ray.dir,ray.org) * 2.f;
-  float C = dot(ray.org,ray.org) - radius*radius;
-
-  float d = B*B - 4.f*A*C;
-  if (d < 0.f) return false;
-
-  d = sqrtf(d);
-
-  float q = B < 0.f ? -0.5f * (B-d) : -0.5f * (B+d);
-
-  float t1 = q/A;
-  float t2 = C/q;
-
-  tnear = fminf(t1,t2);
-  tfar  = fmaxf(t1,t2);
-  return true;
-}
-
-// ========================================================
 // Ray gen prog (woodcock tracking, A+E)
 // ========================================================
 RAYGEN_PROGRAM(woodcockTrackingAE)()
